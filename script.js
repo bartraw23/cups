ccfa941f166e15bebc50bdaee7b8df065c435ef9
Bartek <bartraw23@gmail.com>
2020-08-17 00:35:17 +0200

    Divide projects to externalized files

---
+++ script.js
@@ -0,0 +1,17 @@
+let cups = document.querySelectorAll('.cup');
+
+function cupClick(evt) {
+    let level = Number(this.getAttribute('level'));
+    level += 24;
+    if (level <= 96) {
+        this.style.backgroundPosition = "0 " + level + "%";
+        this.setAttribute('level', level);
+    } else {
+        this.style.cursor = "not-allowed";
+    }
+    console.log(level);
+}
+
+cups.forEach(cup => {
+    cup.addEventListener('click', cupClick);
+})
